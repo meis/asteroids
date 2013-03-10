0ebae9fdde16b9058d6ebfe4ea5756fd104152ab
Marc Egea i Sala <marc.egea.sala@gmail.com>
2013-03-10 21:15:33 +0100

    Now we print a menu when the user uses his 'menu' key

--- src/com/example/asteroids/Asteroids.java
+++ src/com/example/asteroids/Asteroids.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -37,8 +38,19 @@ public class Asteroids extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_asteroids, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+    
+    @Override     
+    public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {		
+			case R.id.about:		
+				launchAbout(null);	
+				break;	
+		}
+		
+		return true; 
     }
     
     public void launchAbout(View view) {
