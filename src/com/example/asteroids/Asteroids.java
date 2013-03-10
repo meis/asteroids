532042debae7a1f6d321b5a5d792baf8f3905ec5
Marc Egea i Sala <marc.egea.sala@gmail.com>
2013-03-10 20:39:19 +0100

    Added the 'about' activity

--- src/com/example/asteroids/Asteroids.java
+++ src/com/example/asteroids/Asteroids.java
@@ -1,8 +1,10 @@
 package com.example.asteroids;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class Asteroids extends Activity {
 
@@ -17,6 +19,11 @@ public class Asteroids extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_asteroids, menu);
         return true;
+    }
+    
+    public void launchAbout(View view) {
+    	Intent i = new Intent(this, About.class);
+    	startActivity(i);
     }
     
 }
